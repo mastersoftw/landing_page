3a223378b0a05401c8ff9244fcd36bb7cef220da
RestGR4 <138226807+RestGR4@users.noreply.github.com>
2024-11-09 13:39:24 -0500

    Probando primeng

--- lp_web_home/src/app/app.module.ts
+++ lp_web_home/src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ServiciosComponent } from './servicios/servicios.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { TabMenuModule } from 'primeng/tabmenu';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ServiciosComponent,
-    ContactoComponent
+    ContactoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
