bbd6791c71c6617324cf6bd6f93267d60d24319a
La_2blea <paltajuan1224@gmail.com>
2024-11-09 08:52:56 -0500

    Se declaran los componentes creados

--- lp_web_home/src/app/app.module.ts
+++ lp_web_home/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServiciosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
