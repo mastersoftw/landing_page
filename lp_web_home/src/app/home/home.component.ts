f3bd9bc62279ccea70b18ddd14facd930ca0d8dd
La_2blea <paltajuan1224@gmail.com>
2024-11-11 16:47:07 -0500

    Se construye el formulario en el .ts

--- lp_web_home/src/app/home/home.component.ts
+++ lp_web_home/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MenuItem } from 'primeng/api';
 
 @Component({
@@ -6,22 +7,37 @@ import { MenuItem } from 'primeng/api';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  formContact: FormGroup;
+  build: FormBuilder;
+  valorRadio: string;
 
   items: MenuItem[];
 
+  constructor( ) {}
+
+
+
   ngOnInit(): void {
+    this.buildForm();
 
     this.items = [
       {label: 'INICIO', icon: 'pi pi-fw pi-home'},
       {label: 'ACERCA DE NOSOTROS', icon: 'pi pi-fw pi-calendar'},
       {label: 'SERVICIOS', icon: 'pi pi-fw pi-pencil'},
       {label: 'CONTACTO', icon: 'pi pi-fw pi-file'},
-  ];
+    ];
+  }
 
-
+  buildForm(): void{
+    this.formContact = this.build.group({
+      nombres: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      correo: ['', Validators.required, Validators.email],
+      comentarios: ['']
+    });
   }
 
 }
